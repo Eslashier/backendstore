49ecc27b04361b9815071ba6d01dd5a1bf3fc390
Eslashier <anfelov@gmail.com>
2022-06-07 22:55:29 -0500

    Entities and DTOs changed UUID and ID

--- src/main/java/com/store/backend/dto/BillDTO.java
+++ src/main/java/com/store/backend/dto/BillDTO.java
@@ -1,4 +1,20 @@
 package com.store.backend.dto;
 
+import com.store.backend.collection.Product;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
 public class BillDTO {
+
+    private String id;
+    private String uid = UUID.randomUUID().toString();
+    private String clientName;
+    private String salesmanName;
+    private List<Product> productListSale;
+    private Integer totalSale;
+
 }
