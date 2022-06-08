fc01abf117e0e69a627fc4da13d0132598d81083
Eslashier <anfelov@gmail.com>
2022-06-07 22:52:40 -0500

    DTO file created and mappers files

---
+++ src/main/java/com/store/backend/mapper/ProviderMapper.java
@@ -0,0 +1,4 @@
+package com.store.backend.mapper;
+
+public class ProviderMapper {
+}
---
+++ src/main/java/com/store/backend/mapper/ReceiptMapper.java
@@ -0,0 +1,11 @@
+package com.store.backend.mapper;
+
+import com.store.backend.collection.Provider;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Component
+@EnableWebFlux
+public class ReceiptMapper {
+
+}
