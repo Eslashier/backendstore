3789d8a74f2dd5e19332cd0eeaef22fcbf9ceb43
Eslashier <anfelov@gmail.com>
2022-06-08 18:14:48 -0500

    product routes and use case files created

---
+++ src/main/java/com/store/backend/routes/product/DeleteProductRoute.java
@@ -0,0 +1,7 @@
+package com.store.backend.routes.product;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DeleteProductRoute {
+}
---
+++ src/main/java/com/store/backend/usecases/product/PostProductUseCase.java
@@ -0,0 +1,4 @@
+package com.store.backend.usecases.product;
+
+public class PostProductUseCase {
+}
