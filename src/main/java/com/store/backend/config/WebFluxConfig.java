5ffe38315893092066d73c3912f53643bf6573a9
Eslashier <anfelov@gmail.com>
2022-06-10 16:04:18 -0500

    added configuration anotation at WebFluxConfig

--- src/main/java/com/store/backend/config/WebFluxConfig.java
+++ src/main/java/com/store/backend/config/WebFluxConfig.java
@@ -1,10 +1,12 @@
 package com.store.backend.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
+@Configuration
 public class WebFluxConfig {
     @Bean
     public WebFluxConfigurer corsConfigure() {
